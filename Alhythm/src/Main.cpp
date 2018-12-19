2a0091cdfcf7cc2cf8de84e019dba259dbc5f066
Shirotsu <albus0sh@gmail.com>
2018-12-19 12:54:18 +0900

    created Siv3D-env on VS2017

---
+++ Alhythm/src/Main.cpp
@@ -0,0 +1,11 @@
+﻿#include <Siv3D.hpp>
+
+void Main(){
+	const Font font(30);
+
+	while (System::Update()){
+		font(L"ようこそ、Siv3D の世界へ！").draw();
+
+		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+	}
+}
